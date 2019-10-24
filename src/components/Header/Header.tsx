b81971932774684cde8f4ad130698eab86673052
Dat Truong <dat.truonghuu1212@gmail.com>
2019-10-25 00:44:45 +0300

    chore: config tslint, prettier, husky, lintstaged

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,49 @@
-import React from 'react';
+import React from 'react'
 import {
-  AppName, Banner, BannerFooter,
-  LinkContainer,
-  Logo,
-  Navigation,
-  StyledHeader,
-  StyledLink,
-} from './style';
+	AppName,
+	Banner,
+	BannerFooter,
+	LinkContainer,
+	Logo,
+	Navigation,
+	StyledHeader,
+	StyledLink,
+} from './style'
 import logo from '../../assets/hel-logo.svg'
 import bannerFooter from '../../assets/banner-footer.png'
 
-const NavLink = (props:any) => (
-    <StyledLink
-        {...props}
-        getProps={({ isCurrent }) => {
-          return {
-            style: {
-              fontWeight: isCurrent ? "bold" : "normal",
-              opacity: isCurrent ? 1 : 0.8
-            }
-          };
-        }}
-    />
-);
+const NavLink = (props: any) => (
+	<StyledLink
+		{...props}
+		getProps={({isCurrent}) => {
+			return {
+				style: {
+					fontWeight: isCurrent ? 'bold' : 'normal',
+					opacity: isCurrent ? 1 : 0.8,
+				},
+			}
+		}}
+	/>
+)
 
-const Header:React.FC = (props) => {
-  return(
-   <>
-     <StyledHeader>
-       <Navigation>
-         <Banner to="/">
-           <Logo src={logo} alt="logo"/>
-           <AppName>Feedback System</AppName>
-         </Banner>
-         <LinkContainer>
-           <NavLink to="/">Home</NavLink>
-           <NavLink to="category">Category</NavLink>
-         </LinkContainer>
-       </Navigation>
-     </StyledHeader>
-     <BannerFooter src={bannerFooter}/>
-   </>
-  )
-};
+const Header: React.FC = props => {
+	return (
+		<>
+			<StyledHeader>
+				<Navigation>
+					<Banner to="/">
+						<Logo src={logo} alt="logo" />
+						<AppName>Feedback System</AppName>
+					</Banner>
+					<LinkContainer>
+						<NavLink to="/">Home</NavLink>
+						<NavLink to="category">Category</NavLink>
+					</LinkContainer>
+				</Navigation>
+			</StyledHeader>
+			<BannerFooter src={bannerFooter} />
+		</>
+	)
+}
 
-export default Header;
+export default Header
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,87 +1,87 @@
-import styled, {css} from 'styled-components';
-import { Link } from "@reach/router"
-import {media} from '../../styles/utils';
+import styled, {css} from 'styled-components'
+import {Link} from '@reach/router'
+import {media} from '../../styles/utils'
 
 const hoverEffect = css`
-   opacity: 0.8;
-    
-   transition: all 0.2s ease-in ;
-    
-   :hover{
-      opacity: 1;
-   }
+	opacity: 0.8;
+
+	transition: all 0.2s ease-in;
+
+	:hover {
+		opacity: 1;
+	}
 `
 
 export const StyledHeader = styled.header`
-    color: ${props => props.theme.colors.white};
-    background-color: ${props => props.theme.appColors.primary}; 
-    padding: 2rem 4rem;
-`;
+	color: ${props => props.theme.colors.white};
+	background-color: ${props => props.theme.appColors.primary};
+	padding: 2rem 4rem;
+`
 
 export const Navigation = styled.nav`
-    display: flex;
-   
-    ${media.phone} {
-       flex-direction: column;
-       align-items: center;
-    }
-`;
+	display: flex;
+
+	${media.phone} {
+		flex-direction: column;
+		align-items: center;
+	}
+`
 
 export const StyledLink = styled(Link)`
-    color: ${props => props.theme.colors.white};
-    font-size: ${props => props.theme.fontSizes.lg};
-    text-decoration-line: none;
-    &:not(:last-of-type) {
-       margin-right: 10.6rem;
-        
-       ${media.phone} {
-        margin-right: 0;
-        margin-bottom: 1.8rem;
-      } 
-    }
-    
-    ${hoverEffect}
-`;
+	color: ${props => props.theme.colors.white};
+	font-size: ${props => props.theme.fontSizes.lg};
+	text-decoration-line: none;
+	&:not(:last-of-type) {
+		margin-right: 10.6rem;
+
+		${media.phone} {
+			margin-right: 0;
+			margin-bottom: 1.8rem;
+		}
+	}
+
+	${hoverEffect}
+`
 
 export const LinkContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-left: auto;
-    
-    ${media.phone} {
-       margin-left: 0;
-       flex-direction: column;
-       margin-top: 1.8rem;
-    }
-`;
+	display: flex;
+	align-items: center;
+	margin-left: auto;
+
+	${media.phone} {
+		margin-left: 0;
+		flex-direction: column;
+		margin-top: 1.8rem;
+	}
+`
 
 export const Banner = styled(Link)`
-   display: flex;
-   cursor: pointer;
-   ${hoverEffect};
-   text-decoration-line: none;
-   color: ${props => props.theme.colors.white};
-   
-    ${media.phone} {
-      flex-direction: column;
-      align-items: center;
-    }
-`;
+	display: flex;
+	cursor: pointer;
+	${hoverEffect};
+	text-decoration-line: none;
+	color: ${props => props.theme.colors.white};
+
+	${media.phone} {
+		flex-direction: column;
+		align-items: center;
+	}
+`
 
 export const Logo = styled.img`
-    height: 4.6rem;
-    width: 10rem;
-`;
+	height: 4.6rem;
+	width: 10rem;
+`
 
 export const AppName = styled.h1`
-    margin-left: 2.2rem;
-`;
+	margin-left: 2.2rem;
+`
 
 export const BannerFooter = styled.img`
-    width: 100%;
-    height: 8.1rem;
-    margin-top: -2px;
-    ${media.phone} {
-       object-fit: cover;
-    }
-`;
+	width: 100%;
+	height: 8.1rem;
+	margin-top: -2px;
+	${media.phone} {
+		object-fit: cover;
+	}
+`
