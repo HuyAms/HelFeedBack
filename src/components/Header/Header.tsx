c2cd9f197d1df1da6c0100a3050abf6e6fc2bb67
Dat Truong <dat.truonghuu1212@gmail.com>
2019-10-28 23:24:55 +0200

    feat: add menu toggle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import {
+	AppHeader,
 	AppName,
 	Banner,
 	BannerFooter,
 	LinkContainer,
 	Logo,
+	MenuIcon,
 	Navigation,
 	StyledHeader,
 	StyledLink,
 } from './style'
 import logo from '../../assets/hel-logo.svg'
 import bannerFooter from '../../assets/banner-footer.png'
+import menuIconSrc from '../../assets/menu-icon.svg'
 
 const NavLink = (props: any) => (
 	<StyledLink
@@ -27,22 +30,33 @@ const NavLink = (props: any) => (
 )
 
 const Header: React.FC = props => {
+	const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
+	const onMenuBtnClicked = () => () => {
+		setIsMenuOpen(isMenuOpen => !isMenuOpen)
+	}
+
 	return (
-		<>
+		<AppHeader>
 			<StyledHeader>
 				<Navigation>
-					<Banner to="/">
+					<MenuIcon onClick={onMenuBtnClicked()} src={menuIconSrc} />
+					<Banner onClick={onMenuBtnClicked()} to="/">
 						<Logo src={logo} alt="logo" />
 						<AppName>Feedback System</AppName>
 					</Banner>
-					<LinkContainer>
-						<NavLink to="/">Home</NavLink>
-						<NavLink to="category">Category</NavLink>
+					<LinkContainer isOpen={isMenuOpen}>
+						<NavLink onClick={onMenuBtnClicked()} to="/">
+							Home
+						</NavLink>
+						<NavLink onClick={onMenuBtnClicked()} to="category">
+							Category
+						</NavLink>
 					</LinkContainer>
 				</Navigation>
 			</StyledHeader>
 			<BannerFooter src={bannerFooter} />
-		</>
+		</AppHeader>
 	)
 }
 
