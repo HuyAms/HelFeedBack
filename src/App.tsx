de1420b95a1ff1d4baeb4d6adc4b179905c75f2d
huy <dinhhuyams@gmail.com>
2019-11-20 22:51:09 +0200

    feat: add notfound component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Router} from '@reach/router'
+import { RouteComponentProps, Router } from "@reach/router";
 import ChannelLayout from './layout/ChannelLayout'
 import Question from './screen/questionPage/Question'
 import Category from './screen/categoryPage/Category'
@@ -7,6 +7,13 @@ import AdminHome from './screen/admin/home'
 import Main from './screen/mainPage/Main'
 import AdminLayout from "./layout/AdminLayout";
 import SignIn from "./screen/auth/signin";
+
+interface Props extends RouteComponentProps{
+
+}
+const NotFound: React.FunctionComponent<Props> = () => {
+  return <p>Channel not found</p>
+}
 
 const App = () => {
 
@@ -21,6 +28,7 @@ const App = () => {
         <Category path="categories" />
         <Question path="categories/:id/questions" />
       </ChannelLayout>
+      <NotFound default={true}/>
     </Router>
 	)
 }
