3b73b63feb022d843559371ffba94540e2c0cbc0
anniluo <anniluo@metropolia.fi>
2019-11-21 16:29:39 +0200

    feat: textfeedback element styling

--- src/screen/textFeedbackPage/TextFeedback.tsx
+++ src/screen/textFeedbackPage/TextFeedback.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {RouteComponentProps} from '@reach/router'
+import {
+	TextFeedbackContainer,
+	FeedbackTextarea,
+	SendTextFeedbackButton,
+} from './style'
 
 interface Props extends RouteComponentProps<{name: string}> {}
 
+//TODO: disable send button if the textarea is empty
+
 const TextFeedback: React.FC<Props> = () => {
 	return (
-		<div>
-			<p>Text feedback</p>
-		</div>
+		<TextFeedbackContainer>
+			<FeedbackTextarea autoFocus placeholder="Message..."></FeedbackTextarea>
+			<SendTextFeedbackButton>Send it</SendTextFeedbackButton>
+		</TextFeedbackContainer>
 	)
 }
 export default TextFeedback
--- src/screen/textFeedbackPage/style.ts
+++ src/screen/textFeedbackPage/style.ts
@@ -1,1 +1,30 @@
 import styled from 'styled-components'
+
+export const TextFeedbackContainer = styled.div``
+
+export const FeedbackTextarea = styled.textarea`
+	display: block;
+	width: 60rem;
+	height: 45rem;
+	margin: auto;
+
+	background: ${props => props.theme.colors.white};
+	border: 1px solid rgba(0, 0, 0, 0.3);
+	border-radius: 1rem;
+	font-size: ${props => props.theme.fontSizes.lg};
+`
+
+export const SendTextFeedbackButton = styled.button`
+	display: block;
+	width: 60rem;
+	height: 8rem;
+	border: none;
+	background: ${props => props.theme.colors.cyan};
+	border-radius: 1rem;
+	margin: auto;
+	margin-top: 3rem;
+
+	text-align: center;
+	font-size: ${props => props.theme.fontSizes.lg};
+	cursor: pointer;
+`
