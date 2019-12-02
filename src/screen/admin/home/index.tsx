eb25c18bfb214d4bd679fa9ce6e20e21ef5058b0
huy <dinhhuyams@gmail.com>
2019-12-02 20:59:19 +0200

    feat: add channel selection

--- src/screen/admin/home/index.tsx
+++ src/screen/admin/home/index.tsx
@@ -1,19 +1,53 @@
 import React from 'react'
+import { Select } from 'antd';
 import {RouteComponentProps} from '@reach/router'
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import { getChannels } from "../../../modules/Channels";
+import ModelState from "../../../models/bases/ModelState";
+import Channel from "../../../models/Channel";
 
 interface Props extends RouteComponentProps {
   getChannels: () => any
+  channels: ModelState<Channel[]>
 }
 
-const AdminHome: React.FunctionComponent<Props> = ({getChannels}) => {
+const {Option} = Select;
+
+const AdminHome: React.FunctionComponent<Props> = ({getChannels, channels}) => {
 
 	React.useEffect(() => {
     getChannels()
 	}, [])
 
-	return <p>This is admin home</p>
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  }
+
+  const renderChannelsSelect = () => {
+
+    if(channels.status === 'success') {
+
+      return (
+        <div>
+          <Select defaultValue={channels.data[0]._id} style={{ width: 200 }} onChange={handleChange}>
+            {channels.data.map(channel => (<Option key={channel._id} value={channel._id}>{channel.name}</Option>))}
+          </Select>
+        </div>
+      )
+    }
+
+    return null
+  }
+
+	return (
+    <>
+      {renderChannelsSelect()}
+    </>
+  )
+}
+
+const mapStateToProps = ({channels}) => {
+  return {channels}
 }
 
 const mapDispatchToProps = {
@@ -21,7 +55,7 @@ const mapDispatchToProps = {
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(AdminHome)
 
